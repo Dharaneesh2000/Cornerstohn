4c3f38f01de88f093d3be8c3a98d9c9ca822107b
Dharaneesh Nandakumar <72527034+Dharaneesh2000@users.noreply.github.com>
2022-06-16 16:37:24 +0530

    sidebar text change

--- src/Admin/Sidebar.js
+++ src/Admin/Sidebar.js
@@ -18,7 +18,7 @@ function AdminSidebar() {
                             <img src={CorrnerstohnLogo} alt="" />
                         </span>
 
-                        <div className="text logo-text">
+                        <div className="text-sidebar logo-text">
                             {/* <span className="name">Codinglab</span>
                             <span className="profession">Web developer</span> */}
                             <img src={CorrnerstohnTitle} alt="" style={{ width: "155px" }} />
@@ -34,70 +34,70 @@ function AdminSidebar() {
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-chart-pie"></i>
-                                    <span className="text nav-text">My Dashboard</span>
+                                    <span className="text-sidebar nav-text">My Dashboard</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-square-h"></i>
-                                    <span className="text nav-text">Practices</span>
+                                    <span className="text-sidebar nav-text">Practices</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-stethoscope" />
-                                    <span className="text nav-text">Doctor</span>
+                                    <span className="text-sidebar nav-text">Doctor</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-bed-pulse"></i>
-                                    <span className="text nav-text">Patients</span>
+                                    <span className="text-sidebar nav-text">Patients</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-magnifying-glass"></i>
-                                    <span className="text nav-text">User Search</span>
+                                    <span className="text-sidebar nav-text">User Search</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-file-lines"></i>
-                                    <span className="text nav-text">Forms</span>
+                                    <span className="text-sidebar nav-text">Forms</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-cloud-arrow-up"></i>
-                                    <span className="text nav-text">Uploads</span>
+                                    <span className="text-sidebar nav-text">Uploads</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-user-tie"></i>
-                                    <span className="text nav-text">Employees</span>
+                                    <span className="text-sidebar nav-text">Employees</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-key"></i>
-                                    <span className="text nav-text">Change Password</span>
+                                    <span className="text-sidebar nav-text">Change Password</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
                                     <i class="icon fa-solid fa-right-from-bracket"></i>
-                                    <span className="text nav-text">Logout</span>
+                                    <span className="text-sidebar nav-text">Logout</span>
                                 </a>
                             </li>
 
@@ -108,7 +108,7 @@ function AdminSidebar() {
                         <li className="">
                             <a href="#">
                                 <i className='bx bx-log-out icon' ></i>
-                                <span className="text nav-text">Logout</span>
+                                <span className="text-sidebar nav-text">Logout</span>
                             </a>
                         </li>
 
@@ -131,7 +131,7 @@ function AdminSidebar() {
 
             <section className="home">
                 <div className='card' style={{ boxShadow: "0px 4px 10px 0px #00000040" }}>
-                    <div className="text" style={{ textAlign: "right", fontSize:"18px" }}>Admin</div>
+                    <div className="text-sidebar" style={{ textAlign: "right", fontSize:"18px" }}>Admin</div>
                 </div>
             </section>
         </React.Fragment>
