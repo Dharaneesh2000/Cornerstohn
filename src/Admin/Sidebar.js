856e2c4e76519bd82fa407091b4003a3fe183c6d
Dharaneesh Nandakumar <72527034+Dharaneesh2000@users.noreply.github.com>
2022-06-16 16:25:10 +0530

    desktop sidebar

--- src/Admin/Sidebar.js
+++ src/Admin/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Sidebar.css"
-import CorrnerstohnLogo from "../Login/cornerstohn.png"
+import CorrnerstohnLogo from "../Admin/Dashoard Icons/cornerstohn logooo.png"
+import CorrnerstohnTitle from "../Admin/Dashoard Icons/cornerstohn word.png"
 function AdminSidebar() {
 
     const toggle = () => {
@@ -18,12 +19,13 @@ function AdminSidebar() {
                         </span>
 
                         <div className="text logo-text">
-                            <span className="name">Codinglab</span>
-                            <span className="profession">Web developer</span>
+                            {/* <span className="name">Codinglab</span>
+                            <span className="profession">Web developer</span> */}
+                            <img src={CorrnerstohnTitle} alt="" style={{ width: "155px" }} />
                         </div>
                     </div>
 
-                    <i className='bx bx-chevron-right toggle' onClick={() => toggle()}></i>
+                    <i className='fa-solid fa-circle-arrow-right toggle sidebaroc' onClick={() => toggle()}></i>
                 </header>
 
                 <div className="menu-bar">
@@ -31,70 +33,70 @@ function AdminSidebar() {
                         <ul className="menu-links">
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-home-alt icon bi-pie-chart-fill' ></i>
+                                    <i class="icon fa-solid fa-chart-pie"></i>
                                     <span className="text nav-text">My Dashboard</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                                    <i class="icon fa-solid fa-square-h"></i>
                                     <span className="text nav-text">Practices</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                                    <i class="icon fa-solid fa-stethoscope" />
                                     <span className="text nav-text">Doctor</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                                    <i class="icon fa-solid fa-bed-pulse"></i>
                                     <span className="text nav-text">Patients</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                                    <i class="icon fa-solid fa-magnifying-glass"></i>
                                     <span className="text nav-text">User Search</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bar-chart-alt-2 icon' ></i>
+                                    <i class="icon fa-solid fa-file-lines"></i>
                                     <span className="text nav-text">Forms</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-bell icon'></i>
+                                    <i class="icon fa-solid fa-cloud-arrow-up"></i>
                                     <span className="text nav-text">Uploads</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-pie-chart-alt icon' ></i>
+                                    <i class="icon fa-solid fa-user-tie"></i>
                                     <span className="text nav-text">Employees</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-heart icon' ></i>
+                                    <i class="icon fa-solid fa-key"></i>
                                     <span className="text nav-text">Change Password</span>
                                 </a>
                             </li>
 
                             <li className="nav-link">
                                 <a href="#">
-                                    <i className='bx bx-wallet icon' ></i>
+                                    <i class="icon fa-solid fa-right-from-bracket"></i>
                                     <span className="text nav-text">Logout</span>
                                 </a>
                             </li>
@@ -128,9 +130,22 @@ function AdminSidebar() {
             </nav>
 
             <section className="home">
-                <div className="text">Dashboard Sidebar</div>
+                <div className='card' style={{ boxShadow: "0px 4px 10px 0px #00000040" }}>
+                    <div className="text" style={{ textAlign: "right", fontSize:"18px" }}>Admin</div>
+                </div>
             </section>
         </React.Fragment>
     )
 }
 export default AdminSidebar;
+
+
+{/* <i class="fa-solid fa-square-h"></i>
+<i class="fa-solid fa-stethoscope"></i>
+<i class="fa-solid fa-bed-pulse"></i>
+<i class="fa-solid fa-magnifying-glass"></i>
+<i class="fa-solid fa-file-lines"></i>
+<i class="fa-solid fa-cloud-arrow-up"></i>
+<i class="fa-solid fa-user-vneck"></i>
+<i class="fa-solid fa-right-from-bracket"></i>
+<i class="fa-solid fa-key"></i> */}
