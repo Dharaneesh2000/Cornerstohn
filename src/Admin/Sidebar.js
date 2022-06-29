0df98c56bdd73869ac14b83ff1f697b4179f8147
Dharaneesh Nandakumar <72527034+Dharaneesh2000@users.noreply.github.com>
2022-06-29 09:21:37 +0530

    Mobile Sidear

--- src/Admin/Sidebar.js
+++ src/Admin/Sidebar.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import "./Sidebar.css"
-import CorrnerstohnLogo from "../Admin/Dashoard Icons/cornerstohn logooo.png"
-import CorrnerstohnTitle from "../Admin/Dashoard Icons/cornerstohn word.png"
+import React, { useState } from 'react';
+import "./Sidebar.css";
+import CorrnerstohnLogo from "../Admin/Dashoard Icons/cornerstohn logooo.png";
+import CorrnerstohnTitle from "../Admin/Dashoard Icons/cornerstohn word.png";
+import CornerstohnMobile from "../Admin/Dashoard Icons/cornerstohn logo.png";
+import MobileMenu from "../Admin/Dashoard Icons/menu.png"
 function AdminSidebar() {
+
 
     const toggle = () => {
         const body = document.querySelector('body'),
             sidebar = body.querySelector('nav');
         sidebar.classList.toggle("close");
     }
+    // Mobile Nav bar
+    // window.addEventListener("load", () => {
+    //     document.body.classList.remove("preload");
+    // });
+    const [sideTgl, setSideTgl] = useState("nav")
+    const mobileSideOpen = () => {
+        setSideTgl("nav nav--open");
+    }
+    const mobileSideClose = () => {
+        setSideTgl("nav");
+    }
+
+
     return (
         <React.Fragment>
             <nav className="sidebar close d-none d-sm-none d-md-block d-lg-block">
@@ -103,28 +119,6 @@ function AdminSidebar() {
 
                         </ul>
                     </div>
-
-                    {/* <div className="bottom-content">
-                        <li className="">
-                            <a href="#">
-                                <i className='bx bx-log-out icon' ></i>
-                                <span className="text-sidebar nav-text">Logout</span>
-                            </a>
-                        </li>
-
-                        <li className="mode">
-                            <div className="sun-moon">
-                                <i className='bx bx-moon icon moon'></i>
-                                <i className='bx bx-sun icon sun'></i>
-                            </div>
-                            <span className="mode-text text">Dark mode</span>
-
-                            <div className="toggle-switch">
-                                <span className="switch"></span>
-                            </div>
-                        </li>
-
-                    </div> */}
                 </div>
 
             </nav>
@@ -136,21 +130,72 @@ function AdminSidebar() {
             </section> */}
 
             {/* Mobile View */}
-            <header className="header-mobile d-block d-sm-block d-md-none d-lg-none">
-                {/* Logo */}
-                <a href="#" className="logo-mobile">LR</a>
-                {/* Hamburger icon */}
-                <input className="side-menu" type="checkbox" id="side-menu" />
-                <label className="hamb" for="side-menu"><span className="hamb-line"></span></label>
-                {/* Menu */}
-                <nav className="nav">
-                    <ul className="menu-mobile">
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Blog</a> </li>
-                        <li><a href="#">About</a></li>
-                    </ul>
+            <div className="d-block d-sm-block d-md-none d-lg-none">
+                <header className="header" style={{ boxShadow: "0px 4px 10px 0px #00000040" }}>
+                    {/* <div className='row'> */}
+                    <div className='col-4 px-2 mt-3'>
+                        <button className="header__button" id="btnNav" type="button" onClick={mobileSideOpen}>
+                            <img src={MobileMenu} />
+                        </button>
+                    </div>
+                    <div className='col-4 text-center py-3'>
+                        <img src={CornerstohnMobile} width="140px" alt="Company Logo" />
+                    </div>
+                    <div className='col-4 px-2 mt-4' style={{ textAlign: "end" }}>
+                        <h6 style={{fontSize:"12px"}}><b>Admin</b></h6>
+                    </div>
+                    {/* </div> */}
+                </header>
+                <nav className={sideTgl}>
+                    <div className="nav__links text-center">
+                        <img src={CornerstohnMobile} width="140px" alt="Company Logo" className="px-2 py-4" style={{ textAlign: "left" }} />
+                        <a href="#" className="nav__link  nav__link--active">
+                            My Dashboard
+                        </a>
+                        <a className="nav__link" href="#">
+                            Practices
+                        </a>
+                        <a className="nav__link" href="#">
+                            Doctor
+                        </a>
+                        <a className="nav__link" href="#">
+                            Patients
+                        </a>
+                        <a className="nav__link" href="#">
+                            User Search
+                        </a>
+                        <a className="nav__link" href="#">
+                            Forms
+                        </a>
+                        <a className="nav__link" href="#">
+                            Uploads
+                        </a>
+                        <a className="nav__link" href="#">
+                            Employees
+                        </a>
+                        <a className="nav__link" href="#">
+                            Change Password
+                        </a>
+                        <a className="nav__link" href="#">
+                            Logout
+                        </a>
+                    </div>
+                    <div className="nav__overlay" onClick={mobileSideClose}></div>
                 </nav>
-            </header>
+                {/* <div className='col-sm-4'>
+                            <img src={CorrnerstohnTitle} alt="Company Logo" />
+                        </div>
+                        <div className='col-sm-4'>
+                            <h4>Admin</h4>
+                        </div> */}
+                {/* <div className='col-sm-4'>
+                        <img src={CorrnerstohnTitle} alt="Company Logo" />
+                    </div>
+                    <div className='col-sm-4'>
+                        <h4>Admin</h4>
+                    </div> */}
+            </div>
+            {/* Mobile view End */}
         </React.Fragment>
     )
 }
