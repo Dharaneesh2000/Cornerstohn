ea1da1ea43a9b1775a54e0f4a2d693aa78deed04
Ajay krishnan creations <81760087+Ajaykrishnancreations@users.noreply.github.com>
2022-07-08 12:04:03 +0530

    froms+uploads page update

--- src/Admin/AdminForms.js
+++ src/Admin/AdminForms.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import "../Login/home.css";
 import AdminSidebar from './Sidebar';
+import AdminHeader from './AdminHeader';
 
 function Forms() {
     return (
         <div className='container-fluid'>
-
+            <AdminHeader />
+            <br></br><br></br>
             <div className='row'>
-                <div className='col-lg-1 d-none d-md-none d-lg-block'>
+                <div className='col-lg-1'>
                     <AdminSidebar />
                 </div>
-
-
                 <div className='col-lg-11'>
                     <br />
                     {/*Add Practice button */}
@@ -20,61 +20,62 @@ function Forms() {
                         Add Form
                     </button>
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                        <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content">
                                 <div class="row">
                                     <div className='col-6'>
-                                    <h5 class="modal-title" id="exampleModalLabel" style={{ color: "#4F4F4F" }}><b>Create Form</b></h5>
+                                        <br />
+                                        <h5 class="modal-title" id="exampleModalLabel" style={{ color: "#4F4F4F", paddingLeft: "15px" }}><b>Create Form</b></h5>
                                     </div>
                                     <div className='col-6'>
-                                    <button type="button" style={{float:"right"}} class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        <br />
+                                        <button type="button" style={{ float: "right", paddingRight: "15px" }} class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                 </div>
                                 <div class="modal-body">
-                                <center>
-                                Title* <input type="text" style={{width:"250px"}} placeholder="Enter Hospital name" className="popInputText"></input>
-                                <br /><br />
-                                <button className='sButton' >save</button>
-                                </center>
+                                    <center>
+                                        Title* <input type="text" style={{ width: "250px" }} placeholder="Enter Hospital name" className="popInputText"></input>
+                                        <br /><br />
+                                        <button className='sButton' >save</button>
+                                    </center>
                                 </div>
-                                
+
                             </div>
                         </div>
                     </div>
                     {/*Add Practice button end */}
                     {/* ----------[table]------------ -----------*/}
-
-          <div className="table-responsive container mt-3">
-            <table class="table table-sm">
-              <thead>
-                <tr>
-                  <th>S.No</th>
-                  <th>Form Name</th>
-                  <th>Status</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>101</td>
-                  <td>john</td>
-                  <td>1/20,covai</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>102</td>
-                  <td>mary@exam.com</td>
-                  <td>john@exam.com</td>
-                </tr>
-              </tbody>
-            </table>
-            <div className="footer">
-              <center>
-                <button className='sButton'>Previous</button> <button className='sButton' >Next</button>
-              </center>
-            </div>
-          </div>
+                    <div className="table-responsive container mt-3">
+                        <table class="table table-sm">
+                            <thead>
+                                <tr>
+                                    <th>S.No</th>
+                                    <th>Form Name</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>101</td>
+                                    <td>john</td>
+                                    <td>1/20,covai</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>102</td>
+                                    <td>mary@exam.com</td>
+                                    <td>john@exam.com</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className="footer">
+                            <center>
+                                <button className='sButton'>Previous</button> <button className='sButton' >Next</button>
+                            </center>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Admin/AdminUploads.js
+++ src/Admin/AdminUploads.js
@@ -1,40 +1,98 @@
 import React from 'react';
 import "../Login/home.css";
 import AdminSidebar from './Sidebar';
+import AdminHeader from './AdminHeader';
 
 function Uploads() {
     return (
-        <div>
-            <AdminSidebar />
-        
-        <div className='' style={{paddingLeft:"8%"}}>
-            <br />
-            {/*Add Practice button */}
-            <input className='popInputText' placeholder='Search' type="text"></input>
-            <button className="sButton" style={{width:"150px",float:"right"}} data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Add Practice
-            </button>
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-fullscreen">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel" style={{ color: "#4F4F4F" }}><b>Create Practice</b></h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className='container-fluid'>
+            <AdminHeader />
+            <br></br><br></br>
+            <div className='row'>
+                <div className='col-lg-1'>
+                    <AdminSidebar />
+                </div>
+                <div className='col-lg-11'>
+                    <br />
+                    {/*Add Practice button */}
+                    <input className='popInputText' placeholder='Search' type="text"></input>
+                    <button className="sButton" style={{ width: "150px", float: "right" }} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Upload image
+                    </button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="row">
+                                    <div className='col-6'>
+                                        <br />
+                                        <h5 class="modal-title" id="exampleModalLabel" style={{ color: "#4F4F4F", paddingLeft: "15px" }}><b>Upload image</b></h5>
+                                    </div>
+                                    <div className='col-6'>
+                                        <br />
+                                        <button type="button" style={{ float: "right", paddingRight: "15px" }} class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                </div>
+                                <div class="modal-body">
+                                    <div className='row'>
+                                        <div className='col-3'>Title*</div>
+                                        <div className='col-9'><input type="text" style={{ width: "250px" }} placeholder="Title" className="popInputText"></input></div>
+                                    </div><br />
+                                    <div className='row'>
+                                        <div className='col-3'>Catergory*</div>
+                                        <div className='col-9'><input type="text" style={{ width: "250px" }} placeholder="Choose" className="popInputText"></input></div>
+                                    </div><br />
+                                    <div className='row'>
+                                        <div className='col-3'>Upload*</div>
+                                        <div className='col-9'><input type="text" style={{ width: "250px" }} placeholder="Upload File" className="popInputText"></input></div>
+                                    </div>
+                                </div><br />
+                                <center>
+                                <button className='sButton' >save</button>
+                                <br/><br/>
+                                </center>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                        Uploads
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button"  class="sButton" style={{width:"anto"}} data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="sButton" style={{width:"anto"}}>Save changes</button>
+                    </div>
+                    {/*Add Practice button end */}
+                    {/* ----------[table]------------ -----------*/}
+                    <div className="table-responsive container mt-3">
+                        <table class="table table-sm">
+                            <thead>
+                                <tr>
+                                    <th>S.No</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Uploaded by</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>101</td>
+                                    <td>john</td>
+                                    <td>covai</td>
+                                    <td>covai</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>102</td>
+                                    <td>mary@exam</td>
+                                    <td>john@exam</td>
+                                    <td>john@exam</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div className="footer">
+                            <center>
+                                <button className='sButton'>Previous</button> <button className='sButton' >Next</button>
+                            </center>
                         </div>
                     </div>
                 </div>
             </div>
-            {/*Add Practice button end */}
-            </div>
-
         </div>
+
     )
 }
 
